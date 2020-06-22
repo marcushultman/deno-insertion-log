72061bc66ddd5501ef839af8898ecf2cd1d55861
Marcus Hultman <marcus.hultman1@gmail.com>
2020-06-22 18:53:15 +0200

    change the API a bit

--- insertion_log.ts
+++ insertion_log.ts
@@ -1,13 +1,11 @@
-type Message = { id: string | null; contents: string };
+import { TextEncoder } from "./web/text_encoding.ts";
+
+type Message = { id?: string; contents: string };
 interface Writer {
   writeSync(p: Uint8Array): number;
 }
 
-function newLines(message: string) {
-  return 1 + (message.match(/\n/g) || []).length;
-}
-
-export default class InsertionLog {
+export class InsertionLog {
   private messages = [] as Message[];
   private encoder = new TextEncoder();
 
@@ -19,26 +17,26 @@ export default class InsertionLog {
     }
   }
 
-  private deleteLineAndGoToBeginning() {
+  private clearLine() {
     this.stream.writeSync(this.encoder.encode(`\u001B[2K\u001B[0G`));
   }
 
-  private getLastMessages(num: number) {
+  private tailMessages(num: number) {
     return this.messages.slice(this.messages.length - num);
   }
 
   private rewindMessages(num: number) {
-    const numLines = this.getLastMessages(num).reduce(
-      (sum, message) => sum + newLines(message.contents),
+    const numLines = this.tailMessages(num).reduce(
+      (sum, { contents }) => sum + 1 + (contents.match(/\n/g) || []).length,
       0,
     );
     this.cursorUp(numLines);
   }
 
   private printMessages(num: number) {
-    for (const message of this.getLastMessages(num)) {
+    for (const message of this.tailMessages(num)) {
       for (const line of message.contents.split(/\n/)) {
-        this.deleteLineAndGoToBeginning();
+        this.clearLine();
         this.stream.writeSync(this.encoder.encode(`${line}\n`));
       }
     }
@@ -53,36 +51,20 @@ export default class InsertionLog {
     throw new Error(`No message for id: ${id}`);
   }
 
-  private logAt(index: number, message: string, id?: string) {
-    this.messages.splice(index, 0, { id: id ?? null, contents: message });
-    const tail = this.messages.length - index;
-    this.rewindMessages(tail - 1);
-    this.printMessages(tail);
-  }
-
   log(message: string, id?: string) {
-    this.messages.push({ id: id ?? null, contents: message });
+    this.messages.push({ id, contents: message });
     this.printMessages(1);
   }
 
-  logAfter(afterId: string, message: string, id?: string) {
-    this.logAt(this.indexOfMessage(afterId) + 1, message, id);
-  }
-
-  logBefore(beforeId: string, message: string, id?: string) {
-    this.logAt(this.indexOfMessage(beforeId), message, id);
-  }
-
-  replace(id: string, message: string) {
+  append(id: string, message: string) {
     const index = this.indexOfMessage(id);
-    if (newLines(message) !== newLines(this.messages[index].contents)) {
-      throw new Error(
-        'Not allowed to repace with a different number of new lines',
-      );
-    }
-    this.messages[index].contents = message;
     const tail = this.messages.length - index;
     this.rewindMessages(tail);
+    this.messages[index].contents += message;
     this.printMessages(tail);
+  }
+
+  flush(id: string) {
+    this.messages = this.messages.slice(this.indexOfMessage(id) + 1);
   }
 }
